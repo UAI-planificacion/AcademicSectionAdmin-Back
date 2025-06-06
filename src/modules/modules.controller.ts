9447d84c73b0390164c67784383ec7e1381e95a4
kevinkeyssx <keyss1700@gmail.com>
2025-06-06 18:04:38 -0400

    Modules Controller added

---
+++ src/modules/modules.controller.ts
@@ -0,0 +1,53 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
+import { ModulesService }   from '@modules/modules.service';
+import { CreateModuleDto }  from '@modules/dto/create-module.dto';
+import { UpdateModuleDto }  from '@modules/dto/update-module.dto';
+
+
+@Controller( 'modules' )
+export class ModulesController {
+    constructor(
+        private readonly modulesService: ModulesService
+    ) {}
+
+
+    @Post()
+    create(
+        @Body() createModuleDto: CreateModuleDto
+    ) {
+        return this.modulesService.create(createModuleDto);
+    }
+
+
+    @Get()
+    findAll() {
+        return this.modulesService.findAll();
+    }
+
+
+    @Get( ':id' )
+    findOne(
+        @Param( 'id' ) id: string
+    ) {
+        return this.modulesService.findOne( +id );
+    }
+
+
+    @Patch( ':id' )
+    update(
+        @Param('id') id: string,
+        @Body() updateModuleDto: UpdateModuleDto
+    ) {
+        return this.modulesService.update( +id, updateModuleDto );
+    }
+
+
+    @Delete( ':id' )
+    remove(
+        @Param( 'id' ) id: string
+    ) {
+        return this.modulesService.remove( +id );
+    }
+
+}
