3933654aa9f074dd3b65ce2b717ed910476c7e1d
kevinkeyssx <keyss1700@gmail.com>
2025-06-06 20:33:04 -0400

    Subjet Controller added

---
+++ src/subjects/subjects.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
+import { SubjectsService } from '@subjects/subjects.service';
+import { CreateSubjectDto } from '@subjects/dto/create-subject.dto';
+import { UpdateSubjectDto } from '@subjects/dto/update-subject.dto';
+
+
+@Controller('subjects')
+export class SubjectsController {
+    constructor(private readonly subjectsService: SubjectsService) {}
+
+    @Post()
+    create(
+        @Body() createSubjectDto: CreateSubjectDto
+    ) {
+        return this.subjectsService.create( createSubjectDto );
+    }
+
+
+    @Get()
+    findAll() {
+        return this.subjectsService.findAll();
+    }
+
+
+    @Get( ':id' )
+    findOne(
+        @Param( 'id' ) id: string
+    ) {
+        return this.subjectsService.findOne( id );
+    }
+
+
+    @Patch( ':id' )
+    update(
+        @Param('id') id: string,
+        @Body() updateSubjectDto: UpdateSubjectDto
+    ) {
+        return this.subjectsService.update( id, updateSubjectDto );
+    }
+
+
+    @Delete( ':id' )
+    remove(
+        @Param('id') id: string
+    ) {
+        return this.subjectsService.remove( id );
+    }
+}
