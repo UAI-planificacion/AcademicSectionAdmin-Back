cfc7639cbaea2dc9e16837a35730ccc3d96e0622
kevinkeyssx <keyss1700@gmail.com>
2025-06-06 20:15:09 -0400

    New modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ModulesModule } from './modules/modules.module';
+import { ProfessorsModule } from './professors/professors.module';
+import { PeriodsModule } from './periods/periods.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { DaysModule } from './days/days.module';
+import { SizesModule } from './sizes/sizes.module';
+import { SpacesModule } from './spaces/spaces.module';
 
 import { AppController }    from '@app/app.controller';
 import { ConfigModule }     from '@config/config.module';
@@ -10,7 +16,13 @@ import { SectionsModule }   from '@sections/sections.module';
     imports     : [
         ConfigModule,
         SectionsModule,
-        ModulesModule
+        ModulesModule,
+        ProfessorsModule,
+        PeriodsModule,
+        SubjectsModule,
+        DaysModule,
+        SizesModule,
+        SpacesModule
     ],
     controllers : [AppController],
     providers   : [],
