f7b3e0bdace59eee6c4028df5a78ef194c5a29fd
kevinkeyssx <keyss1700@gmail.com>
2025-06-06 20:32:09 -0400

    Spaces Module added

---
+++ src/spaces/spaces.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { SpacesService }    from '@spaces/spaces.service';
+import { SpacesController } from '@spaces/spaces.controller';
+
+
+@Module({
+    controllers : [SpacesController],
+    providers   : [SpacesService],
+})
+export class SpacesModule {}
