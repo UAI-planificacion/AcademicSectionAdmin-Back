88c7cba08dee1076c75ea22bc9060ae7ca05326f
kevinkeyssx <keyss1700@gmail.com>
2025-06-06 20:30:24 -0400

    Sized Controller added (dont work yet)

---
+++ src/sizes/sizes.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
+import { SizesService }     from '@sizes/sizes.service';
+import { CreateSizeDto }    from '@sizes/dto/create-size.dto';
+import { UpdateSizeDto }    from '@sizes/dto/update-size.dto';
+
+
+@Controller('sizes')
+export class SizesController {
+    constructor(private readonly sizesService: SizesService) {}
+
+    @Post()
+    create(@Body() createSizeDto: CreateSizeDto) {
+        return this.sizesService.create(createSizeDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.sizesService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.sizesService.findOne(id );
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateSizeDto: UpdateSizeDto) {
+        return this.sizesService.update( id, updateSizeDto );
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.sizesService.remove( id );
+    }
+}
