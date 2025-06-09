40acecbb3475a9adf7cc393e48c035cd8041752e
kevinkeyssx <keyss1700@gmail.com>
2025-06-09 18:47:31 -0400

    Sizes controller updated

--- src/sizes/sizes.controller.ts
+++ src/sizes/sizes.controller.ts
@@ -1,36 +1,53 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 
-import { SizesService }     from '@sizes/sizes.service';
-import { CreateSizeDto }    from '@sizes/dto/create-size.dto';
-import { UpdateSizeDto }    from '@sizes/dto/update-size.dto';
+import { SizesService }                 from '@sizes/sizes.service';
+import { CreateSizeDto, SizeValueDto }  from '@sizes/dto/create-size.dto';
+import { UpdateSizeDto }                from '@sizes/dto/update-size.dto';
 
 
-@Controller('sizes')
+@Controller( 'sizes' )
 export class SizesController {
-    constructor(private readonly sizesService: SizesService) {}
+    constructor(
+        private readonly sizesService: SizesService
+    ) {}
+
 
     @Post()
-    create(@Body() createSizeDto: CreateSizeDto) {
-        return this.sizesService.create(createSizeDto);
+    create(
+        @Body() createSizeDto: CreateSizeDto
+    ) {
+        return this.sizesService.create( createSizeDto );
     }
+
 
     @Get()
     findAll() {
         return this.sizesService.findAll();
     }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
+
+    @Get( ':id' )
+    findOne(
+        @Param( 'id' ) id: SizeValueDto
+    ) {
         return this.sizesService.findOne(id );
     }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateSizeDto: UpdateSizeDto) {
+
+    @Patch( ':id' )
+    update(
+        @Param( 'id' ) id: SizeValueDto,
+        @Body() updateSizeDto: UpdateSizeDto
+    ) {
         return this.sizesService.update( id, updateSizeDto );
     }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
+
+    @Delete( ':id' )
+    remove(
+        @Param( 'id' ) id: SizeValueDto
+    ) {
         return this.sizesService.remove( id );
     }
+
 }
