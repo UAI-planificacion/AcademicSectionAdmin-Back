64bb9fb1e414f61636344e9000e35c78032ce166
kevinkeyssx <keyss1700@gmail.com>
2025-06-09 18:47:46 -0400

    Sizes services updated

--- src/sizes/sizes.service.ts
+++ src/sizes/sizes.service.ts
@@ -1,53 +1,77 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { CreateSizeDto } from './dto/create-size.dto';
-import { UpdateSizeDto } from './dto/update-size.dto';
-import { PrismaClient } from '@prisma/client';
-import { PrismaException } from '@app/config/prisma-catch';
+
+import { PrismaClient, SizeValue as PrismaSizeValue } from '@prisma/client';
+
+import { PrismaException }              from '@app/config/prisma-catch';
+import { CreateSizeDto, SizeValueDto }  from '@sizes/dto/create-size.dto';
+import { UpdateSizeDto }                from '@sizes/dto/update-size.dto';
+
 
 @Injectable()
 export class SizesService extends PrismaClient implements OnModuleInit {
-    onModuleInit() {
-        this.$connect();
+    async onModuleInit() {
+        await this.$connect();
     }
 
 
-    async create(createSizeDto: CreateSizeDto) {
-        // try {
-        //     const size = await this.size.create({
-        //         data: createSizeDto
-        //     });
-        //     return size;
-        // } catch (error) {
-        //     throw PrismaException.catch( error );
-        // }
+    #generateDetail( dto: CreateSizeDto | UpdateSizeDto ): string {
+        const { min, max, lessThan, greaterThan } = dto;
+
+        if ( lessThan )     return `< ${lessThan}`;
+        if ( greaterThan )  return `> ${greaterThan}`;
+
+        return `${min} - ${max}`;
     }
+
+
+    async create( createSizeDto: CreateSizeDto ) {
+        try {
+            const size = await this.size.create({
+                data:  {
+                    ...createSizeDto,
+                    id: createSizeDto.id as PrismaSizeValue,
+                    detail: this.#generateDetail( createSizeDto ),
+                },
+            });
+            return size;
+        } catch ( error ) {
+            throw PrismaException.catch( error );
+        }
+    }
+
 
     findAll() {
         return this.size.findMany({});
     }
 
-    findOne(id: string) {
-        // return this.size.findUnique({ where: { id } });
+
+    findOne( id: SizeValueDto ) {
+        return this.size.findUnique({ where: { id }});
     }
 
-    async update(id: string, updateSizeDto: UpdateSizeDto) {
-        // try {
-        //     const size = await this.size.update({
-        //         where: { id },
-        //         data: updateSizeDto
-        //     });
-        //     return size;
-        // } catch (error) {
-        //     throw PrismaException.catch( error );
-        // }
+
+    async update( id: SizeValueDto, updateSizeDto: UpdateSizeDto ) {
+        try {
+            const size = await this.size.update({
+                where: { id },
+                data: {
+                    ...updateSizeDto,
+                    detail: this.#generateDetail( updateSizeDto ),
+                }
+            });
+            return size;
+        } catch ( error ) {
+            throw PrismaException.catch( error );
+        }
     }
 
-    remove(id: string) {
-        // try {
-        //     const size = this.size.delete({ where: { id } });
-        //     return size;
-        // } catch (error) {
-        //     throw PrismaException.catch( error );
-        // }
+
+    async remove( id: SizeValueDto ) {
+        try {
+            const size = await this.size.delete({ where: { id }});
+            return size;
+        } catch ( error ) {
+            throw PrismaException.catch( error );
+        }
     }
 }
