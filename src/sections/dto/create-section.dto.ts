8efb7bf2efabb303ac2f8d549210730830d90c10
kevinkeyssx <keyss1700@gmail.com>
2025-06-11 21:42:43 -0400

    Max 1000 sections create DTO

--- src/sections/dto/create-section.dto.ts
+++ src/sections/dto/create-section.dto.ts
@@ -20,7 +20,7 @@ export class CreateSectionDto {
     @Type(() => Number)
     @IsInt({ message: 'Section code must be an integer.' })
     @Min(1, { message: 'Section code must be a positive integer.' })
-    @Max( 10, { message: 'Section code cannot exceed 10.' } )
+    @Max( 1000, { message: 'Section code cannot exceed 1000.' } )
     @IsNotEmpty({ message: 'Section code is required.' })
     code: number;
 
