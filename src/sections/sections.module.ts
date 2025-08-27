5c7fc08323984cf0d9dab39e160823566fe72772
kevinkeyssx <keyss1700@gmail.com>
2025-08-26 22:59:45 -0400

    SubjectsService in providers implemented

--- src/sections/sections.module.ts
+++ src/sections/sections.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { SectionsService } from './sections.service';
-import { SectionsController } from './sections.controller';
+
+import { SectionsService }		from '@sections/sections.service';
+import { SectionsController }   from '@sections/sections.controller';
+import { SubjectsService }      from '@sections/services/subjects.service';
+
 
 @Module({
-    controllers: [SectionsController],
-    providers  : [SectionsService],
+	controllers	: [SectionsController],
+	providers	: [SectionsService, SubjectsService],
 })
 export class SectionsModule {}
